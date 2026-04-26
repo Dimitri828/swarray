a8683988437560c3ac68e2d7483666ccc58c7a95
Dimitri <dimitrifortini91@gmail.com>
2026-04-26 12:33:17 +0200

    Eseguito selfwork array 1

---
+++ main.js
@@ -0,0 +1,11 @@
+// SELFWORK 1 ARRAY
+
+function crescente(array) {
+return array.sort((a,b)=>a-b)
+}
+
+let cre=[12,43,54,63,234,1,4,656,24]
+console.log(cre);
+
+let crescent= crescente([3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25])
+console.log(crescent);
