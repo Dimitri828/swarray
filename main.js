262151d4e4ef0543242a262ac647183a04b41273
Dimitri <dimitrifortini91@gmail.com>
2026-04-26 13:45:44 +0200

    completato selfwork 2 array

--- main.js
+++ main.js
@@ -1,11 +1,32 @@
 // SELFWORK 1 ARRAY
 
-function crescente(array) {
-return array.sort((a,b)=>a-b)
+// function crescente(array) {
+// return array.sort((a,b)=>a-b)
+// };
+
+// let cre=[12,43,54,63,234,1,4,656,24]
+// console.log(cre);
+
+// let crescent= crescente([3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25])
+// console.log(crescent);
+
+// SELFWORK 2 ARRAY
+
+function media(array) {
+    let somma= array.reduce((acc,n)=>acc+n)
+    let media= somma/array.length
+    let filter = array.filter((number)=> number < media)
+    let maggiore= array.filter((number)=> number> media).length;
+    let minore= array.filter((number)=> number< media).length;
+    console.log(minore);
+    
+    
+    
+    return `La media dei numeri è ${media},i valori minori sono [${filter}],i numeri inferiori alla media sono ${minore},i numeri superiori alla media sono ${maggiore}`
+    
 }
 
-let cre=[12,43,54,63,234,1,4,656,24]
-console.log(cre);
+let exercise= media([1,2,3,4,5,6,7])
 
-let crescent= crescente([3, 7, -2, 5, 8, 1, 2, 5, 6, -4, 12, 25])
-console.log(crescent);
+console.log(exercise);
+
